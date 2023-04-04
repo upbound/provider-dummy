24b7ea8bcd90189fec142e3e77931f95a062071e
Muvaffak Onus <me@muvaf.com>
2023-04-04 03:31:44 +0300

    server: add server code
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

---
+++ cmd/server/main.go
@@ -0,0 +1,103 @@
+/*
+Copyright 2023 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// This is a very simple server that stores and retrieves robots to a map that
+// will be lost when the server is restarted.
+
+type Robot struct {
+	Name  string `json:"name"`
+	Color string `json:"color"`
+}
+
+var robots = make(map[string]Robot)
+
+func main() {
+	http.HandleFunc("/robots", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			name := r.URL.Query().Get("name")
+			robot, ok := robots[name]
+			if !ok {
+				http.Error(w, "Robot not found", http.StatusNotFound)
+				return
+			}
+			if err := json.NewEncoder(w).Encode(robot); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		case "POST":
+			var robot Robot
+			err := json.NewDecoder(r.Body).Decode(&robot)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			if _, ok := robots[robot.Name]; ok {
+				http.Error(w, "Robot already exists", http.StatusBadRequest)
+				return
+			}
+			robots[robot.Name] = robot
+			w.WriteHeader(http.StatusCreated)
+		case "PUT":
+			name := r.URL.Query().Get("name")
+			if name == "" {
+				http.Error(w, "Missing name parameter", http.StatusBadRequest)
+				return
+			}
+			var robot Robot
+			err := json.NewDecoder(r.Body).Decode(&robot)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			if _, ok := robots[name]; !ok {
+				http.Error(w, "Robot not found", http.StatusNotFound)
+				return
+			}
+			delete(robots, name)
+			robots[robot.Name] = robot
+			w.WriteHeader(http.StatusOK)
+		case "DELETE":
+			name := r.URL.Query().Get("name")
+			if name == "" {
+				http.Error(w, "Missing name parameter", http.StatusBadRequest)
+				return
+			}
+			if _, ok := robots[name]; !ok {
+				http.Error(w, "Robot not found", http.StatusNotFound)
+				return
+			}
+			delete(robots, name)
+			w.WriteHeader(http.StatusOK)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+	log.Println("Listening on :8080")
+	log.Printf("Example commands:\n")
+	log.Printf("Create: curl -XPOST -H \"Content-type: application/json\" -d '{ \"name\": \"myrobot\", \"color\": \"green\"}' 'http://127.0.0.1:8080/robots'\n")
+	log.Printf("Get: curl 'http://127.0.0.1:8080/robots?&name=myrobot'\n")
+	log.Printf("Update: curl -XPUT -H \"Content-type: application/json\" -d '{ \"name\": \"myrobot\", \"color\": \"yellow\"}' 'http://127.0.0.1:8080/robots?&name=myrobot'\n")
+	log.Printf("Delete: curl -XDELETE 'http://127.0.0.1:8080/robots?&name=myrobot'\n")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
--- go.mod
+++ go.mod
@@ -66,9 +66,10 @@ require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/mitchellh/copystructure v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
@@ -84,27 +85,30 @@ require (
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
 	github.com/spf13/afero v1.8.0 // indirect
 	github.com/spf13/cobra v1.4.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.19.1 // indirect
-	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
+	golang.org/x/crypto v0.5.0 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
-	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
 	golang.org/x/tools v0.1.12 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20220502173005-c8bf987b8c21 // indirect
 	google.golang.org/grpc v1.47.0 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
