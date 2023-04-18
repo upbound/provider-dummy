257c1ee2c106316a6ad687b7d1550d891c309d50
Muvaffak Onus <me@muvaf.com>
2023-04-18 15:35:13 +0300

    crd: rename group to have upbound.io suffix instead of crossplane.io
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- apis/iam/v1alpha1/groupversion_info.go
+++ apis/iam/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains the v1alpha1 group Sample resources of the Dummy provider.
 // +kubebuilder:object:generate=true
-// +groupName=iam.dummy.crossplane.io
+// +groupName=iam.dummy.upbound.io
 // +versionName=v1alpha1
 package v1alpha1
 
@@ -27,7 +27,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "iam.dummy.crossplane.io"
+	Group   = "iam.dummy.upbound.io"
 	Version = "v1alpha1"
 )
 
