b23bab652bccf8f3d978fc86ebd8d03e7b98568f
Muvaffak Onus <me@muvaf.com>
2023-04-18 16:50:28 +0300

    crd: more renames to have upbound.io suffix instead of crossplane.io
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- apis/v1alpha1/groupversion_info.go
+++ apis/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains the core resources of the Dummy provider.
 // +kubebuilder:object:generate=true
-// +groupName=dummy.crossplane.io
+// +groupName=dummy.upbound.io
 // +versionName=v1alpha1
 package v1alpha1
 
@@ -27,7 +27,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "dummy.crossplane.io"
+	Group   = "dummy.upbound.io"
 	Version = "v1alpha1"
 )
 
